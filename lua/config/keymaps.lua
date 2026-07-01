fde9d68054e201e91b85005b52d79f16d102d923
Dan Shernicoff <dshernicoff@tenable.com>
2026-07-01 09:41:41 -0400

    Add jj keymap for formatting JSON files.

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,3 +3,4 @@
 -- Add any additional keymaps here
 
 vim.keymap.set("i", "qw", "<Esc>", { noremap = true, silent = true })
+vim.keymap.set("n", "jj", ":%!jq <Enter>", { noremap = true, silent = true }) -- Use "jj" to format a JSON file.
