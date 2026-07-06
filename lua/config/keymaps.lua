4a0ee9e718d24caa058aa735bbe1860ea8ca5427
Dan Shernicoff <dshernicoff@tenable.com>
2026-07-06 08:56:04 -0400

    Set w to noautocmd write

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -4,3 +4,6 @@
 
 vim.keymap.set("i", "qw", "<Esc>", { noremap = true, silent = true })
 vim.keymap.set("n", "jj", ":%!jq <Enter>", { noremap = true, silent = true }) -- Use "jj" to format a JSON file.
+vim.api.nvim_create_user_command("w", function()
+  vim.cmd("noautocmd write")
+end, {})
